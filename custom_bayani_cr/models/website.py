f06a986980f2a1a7487df6b7ee97effdc70ef3b1
kalarianayan <nayan@candidroot.com>
2024-06-12 10:03:33 +0530

    [FIX] hidden Add To cart section on portal user issue

--- custom_bayani_cr/models/website.py
+++ custom_bayani_cr/models/website.py
@@ -1,7 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.http import request
 
 
 class Website(models.Model):
     _inherit = "website"
 
     is_search_panel_shown_in_website = fields.Boolean(string='Search Panel Shown in Website')
+
+    @api.model
+    def is_public_user(self):
+        res = super(Website, self).is_public_user()
+        model = request.env['ir.ui.view'].sudo().search([('key', '=', 'website_sale.product')], limit=1)
+        model_name = model.name
+        model.sudo().write({
+            'name': model_name,
+        })
+        return res
+
+
